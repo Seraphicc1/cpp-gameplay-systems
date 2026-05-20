d179a0ce14d96a97e01aef5d7f89b06ae07471e0
Seraphicc1 <seyhan.pc@outlook.de>
2026-05-20 15:57:28 +0200

    Added some functions and notes
    - Add Item functionality
    - prepared everything else for next session

--- systems/inventory/inventory/inventory.cpp
+++ systems/inventory/inventory/inventory.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
+#include <vector>
 
-int main()
+std::string addItem(std::string item, std::vector<std::string>& inventory, int maxSlots);
+std::string removeItem(std::string item, std::vector<std::string>& inventory);
+std::string viewInventory(std::vector<std::string>& inventory);
+std::string clearInventory(std::vector<std::string>& inventory);
+
+int main() // Implement categories and sourrounding systems later, (open backpack?), (equip item? use item?) (item durability)
 {
-    std::cout << "Hello World!\n";
+    std::vector<std::string> inventory;
+    int maxSlots = 5;
+    std::string action;
+
+    std::cout << "*********************** Inventory System ***********************\n";
+    std::cout << "What would you like to do? (add/remove/view/clear/exit): ";
+    std::cin >> action;
+
+    while()
+
+    return 0;
+}
+
+std::string addItem(std::string item, std::vector<std::string>& inventory, int maxSlots){
+    if (maxSlots > inventory.size()) {
+        inventory.push_back(item);
+        maxSlots--;
+        
+        return item + " added to inventory.\n";
+    }
+}
+std::string removeItem(std::string item, std::vector<std::string>& inventory) {
+    // Find item in inventory
+    // Remove item and clear slot
+
+    return item + " removed from inventory.\n";
+}
+std::string viewInventory(std::vector<std::string>& inventory) {
+    // Go throug inventory and print items
+}
+std::string clearInventory(std::vector<std::string>& inventory) {
+    // Clear inventory and reset max slots
+    // Return message
 }
