b62780f30b67fcb80df860ee953809829caf3990
Seraphicc1 <seyhan.pc@outlook.de>
2026-05-27 12:12:28 +0200

    Inventory System V1
    Simple but functional. Features add, drop, open and empty function and uses a string for item.
    
    Next step: Use struct for the item and also add more expansion like stacks, use item, durability and pick-up-from-ground-system

--- systems/inventory/inventory/inventory.cpp
+++ systems/inventory/inventory/inventory.cpp
@@ -1,44 +1,98 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
-std::string addItem(std::string item, std::vector<std::string>& inventory, int maxSlots);
-std::string removeItem(std::string item, std::vector<std::string>& inventory);
-std::string viewInventory(std::vector<std::string>& inventory);
-std::string clearInventory(std::vector<std::string>& inventory);
+void addItem(std::vector<std::string>& inventory, const size_t maxSlots);
+void dropItem(std::vector<std::string>& inventory);
+void openBackpack(const std::vector<std::string>& inventory);
+void emptyBackpack(std::vector<std::string>& inventory);
 
-int main() // Implement categories and sourrounding systems later, (open backpack?), (equip item? use item?) (item durability)
+int main() // Implement categories and sourrounding systems later, (equip item? use item?) (item durability)
 {
     std::vector<std::string> inventory;
-    int maxSlots = 5;
     std::string action;
+    size_t maxSlots = 5;
 
     std::cout << "*********************** Inventory System ***********************\n";
-    std::cout << "What would you like to do? (add/remove/view/clear/exit): ";
-    std::cin >> action;
 
-    while()
+    while (true) {
+        std::cout << "What would you like to do? (add/drop/open/empty/exit): ";
+        std::getline(std::cin, action);
 
+        if (action == "exit") {
+            break;
+        }
+        else if (action == "add") {
+            std::cout << "****************************************************************\n";
+
+            addItem(inventory, maxSlots);
+        }
+        else if (action == "drop") {
+            std::cout << "****************************************************************\n";
+
+            dropItem(inventory);
+        }
+        else if (action == "open") {
+            std::cout << "****************************************************************\n";
+
+            openBackpack(inventory);
+        }
+        else if (action == "empty") {
+            std::cout << "****************************************************************\n";
+
+            emptyBackpack(inventory);
+        }
+        else {
+            std::cout << "****************************************************************\n";
+            std::cout << "Please choose a correct action.\n";
+        }
+    }
+    
     return 0;
 }
 
-std::string addItem(std::string item, std::vector<std::string>& inventory, int maxSlots){
-    if (maxSlots > inventory.size()) {
+void addItem(std::vector<std::string>& inventory, const size_t maxSlots) {
+    if (inventory.size() < maxSlots) {
+        std::cout << "Which item would you like to put in your backpack?: ";
+        std::string item;
+        std::getline(std::cin, item);
+
         inventory.push_back(item);
-        maxSlots--;
-        
-        return item + " added to inventory.\n";
+
+        std::cout << item << " added to your backpack.\n";
+    }
+    else {
+        std::cout << "Your inventory is full.\n";
     }
 }
-std::string removeItem(std::string item, std::vector<std::string>& inventory) {
-    // Find item in inventory
-    // Remove item and clear slot
+void dropItem(std::vector<std::string>& inventory) {
+    std::cout << "Which item would you like to remove from your Backpack?: ";
+    std::string item;
+    std::getline(std::cin, item);
 
-    return item + " removed from inventory.\n";
+    for (auto it = inventory.begin(); it != inventory.end(); ++it) {
+        if (*it == item) {
+            inventory.erase(it);
+            std::cout << item << " dropped on the ground.\n";
+
+            return;
+        }
+    }
+    std::cout << "Item not found.\n";
 }
-std::string viewInventory(std::vector<std::string>& inventory) {
-    // Go throug inventory and print items
+void openBackpack(const std::vector<std::string>& inventory) {
+    if (inventory.empty()) {
+        std::cout << "Your backpack is empty.\n";
+        return;
+    }
+    else {
+        std::cout << "Your backpack contains:\n";
+        for (const auto& item : inventory) {
+            std::cout << "- " << item << std::endl;
+        }
+    }
 }
-std::string clearInventory(std::vector<std::string>& inventory) {
-    // Clear inventory and reset max slots
-    // Return message
+void emptyBackpack(std::vector<std::string>& inventory) {
+    std::cout << "Backpack cleared.\n";
+    inventory.clear();
 }
